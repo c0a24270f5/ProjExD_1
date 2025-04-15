34a966ca184dfbe4163322d0460e360c9122f7bd
yuki <c0a24270f5@edu.teu.ac.jp>
2025-04-15 15:50:11 +0900

    練習６：背景を動かす

--- pygame_image.py
+++ pygame_image.py
@@ -16,8 +16,9 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+        x=tmr
 
-        screen.blit(bg_img, [0, 0])
+        screen.blit(bg_img, [-x, 0])
         screen.blit(kk_img, [300, 200])
         pg.display.update()
         tmr += 1        
