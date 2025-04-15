754556890f2a8cf9ac53a95e651996abc775dc5d
yuki <c0a24270f5@edu.teu.ac.jp>
2025-04-15 17:14:25 +0900

    演習２

--- pygame_image.py
+++ pygame_image.py
@@ -20,16 +20,23 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         key_lst = pg.key.get_pressed()
+        x3=0
+        y=0
         if key_lst[pg.K_UP]:
-            kk_rct.move_ip((0,-1))
+            x3=0
+            y=-1
         if key_lst[pg.K_DOWN]:
-            kk_rct.move_ip((0,+1))
+            x3=0
+            y=+1
         if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip((-1,0))
+            x3=-1
+            y=0
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip((+2,0))
+            x3=+2
+            y=0
         x=tmr%3200
         x2=1
+        k_move=kk_rct.move_ip((x3,y))
         kk_rct.move_ip((-x2,0))
         screen.blit(bg_img, [-x, 0])
         screen.blit(bg_img2, [-x+1600, 0])
