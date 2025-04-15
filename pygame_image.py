4c2987e0b7e5916b3be0130fd8d815069f88d738
yuki <c0a24270f5@edu.teu.ac.jp>
2025-04-15 15:57:06 +0900

    練習７：背景画像を２つにした

--- pygame_image.py
+++ pygame_image.py
@@ -17,8 +17,8 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         x=tmr
-
         screen.blit(bg_img, [-x, 0])
+        screen.blit(bg_img, [-x+1600, 0])
         screen.blit(kk_img, [300, 200])
         pg.display.update()
         tmr += 1        
